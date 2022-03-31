67ee8c6641e1b1159c6f10c70904f9c155842dd2
SofiaCirelli <101577333+SofiaCirelli@users.noreply.github.com>
2022-03-31 09:03:03 -0300

    Exercise 5 completed

---
+++ Assets/Scripts/Ej06.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//6. Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+//en una variable llamada num1 y muestre un mensaje por pantalla indicando "el número es
+//par" "el número es impar." Deberá utilizar el operador “módulo” es el caracter %.
+
+public class Ej06 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
